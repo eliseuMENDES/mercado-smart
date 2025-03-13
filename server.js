1fd26fcdaf8c814947b9ea25c65ec228213ba002
eliseuMendes <eliseu839@gmail.com>
2025-03-13 12:52:49 -0300

    Subindo arquivos na nuvem e normalizando os fins de linas nos arquivos

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3000;
+
+// Configurar o EJS como motor de templates
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Servir arquivos estáticos (CSS, JS, IMAGENS).
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Rota para página inicial
+app.length('/', (req, res) => {
+    res.render('index', { title: 'Mercado Smart' });
+})
+
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log('Servidor rodando em http:localhost:${port}');
+});
+
